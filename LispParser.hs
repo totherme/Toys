652c725405bad947d64976eb87fd3ea0216511e6
Gareth Smith <gds@doc.ic.ac.uk>
2014-11-06 20:19:43 +0000

    LispParser: function skeletons

--- LispParser.hs
+++ LispParser.hs
@@ -1,7 +1,35 @@
 module LispParser where
 
+import Text.ParserCombinators.Parsec
+
+magic :: a
+magic = error "Not implemented yet."
+
 type LSymbol = String
 
 data LAtom = LSymbol LSymbol | LString String | LInt Int deriving Show
 
 data LProg = LAtom LAtom | LList [LProg] | LQuote LProg deriving Show
+
+lispParser :: Parser LProg
+lispParser = magic
+
+latom :: Parser LAtom
+latom = magic
+
+lsymbol :: Parser LSymbol
+lsymbol = magic
+
+lstring :: Parser String
+lstring = magic
+
+lint :: Parser Int
+lint = magic
+
+llist :: Parser [LProg]
+llist = magic
+
+lquote :: Parser LProg
+lquote = magic
+  
+                                                                 
