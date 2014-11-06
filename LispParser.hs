d445f1e02b6957a3c997b0063178c20d1e05b062
Gareth Smith <gds@doc.ic.ac.uk>
2014-11-06 20:14:38 +0000

    AST for a simple lisp

---
+++ LispParser.hs
@@ -0,0 +1,7 @@
+module LispParser where
+
+type LSymbol = String
+
+data LAtom = LSymbol LSymbol | LString String | LInt Int deriving Show
+
+data LProg = LAtom LAtom | LList [LProg] | LQuote LProg deriving Show
