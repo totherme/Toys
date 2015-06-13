87e3f1f3c2b8bd3db5741002943fb6287c723bd1
Gareth Smith <gareth@totherme.org>
2015-06-13 16:12:22 +0100

    Tidy

--- Bot/src/org/totherme/bot/BunnyListener.java
+++ Bot/src/org/totherme/bot/BunnyListener.java
@@ -16,39 +16,70 @@ import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.types.GenericMessageEvent;
 
 /**
- * @author gds
+ * This is the main guts of the bot. There are callback methods which fire whenever interesting things happen -- such as when we recieve messages.
  * 
+ * @author gds
  */
 public class BunnyListener<T extends PircBotX> extends ListenerAdapter<T> {
 	
 	/**
-	 * A class to represent a single saved message.
+	 * A single saved message.
 	 * @author gds
 	 *
 	 */
 	private class SavedMessage {
 		private String sender;
 		private String msg;
+		/**
+		 * 
+		 * @param sender the nick of the person sending this message
+		 * @param msg the message itself
+		 */
 		public SavedMessage(String sender, String msg) {
 			this.sender = sender;
 			this.msg = msg;
 		}
+		/**
+		 * 
+		 * @return the nick of the person who sent this message
+		 */
 		public String getSender() { return sender ; }
+		/**
+		 * 
+		 * @return the message itself
+		 */
 		public String getMessage() { return msg ; }
 	}
 	
+	/**
+	 * A message store
+	 * @author gds
+	 *
+	 */
 	private class MessageMap {
 		private Map<String, List<SavedMessage>> map = new HashMap<String,List<SavedMessage>>();
 		
-		public List<SavedMessage> lookupDestructive(String recipiant) {
-			if (map.containsKey(recipiant)) {
-				List<SavedMessage> msgs = map.get(recipiant);
-				map.remove(recipiant);
+		/**
+		 * Look up what messages might be waiting for some recipient in this store, return those messages, and remove them from the store.
+		 * 
+		 * @param recipient the nick of the person the messages might be addressed to.
+		 * @return all messages addressed to recipient
+		 */
+		public List<SavedMessage> lookupDestructive(String recipient) {
+			if (map.containsKey(recipient)) {
+				List<SavedMessage> msgs = map.get(recipient);
+				map.remove(recipient);
 				return msgs;
 			}
 			else return new ArrayList<SavedMessage>();
 		}
 		
+		/**
+		 * Leave a message for recipient in this store.
+		 * 
+		 * @param recipiant
+		 * @param msg
+		 */
 		public void addMsg(String recipiant, SavedMessage msg) {
 			if (map.containsKey(recipiant)) map.get(recipiant).add(msg);
 			else {
@@ -89,17 +120,17 @@ public class BunnyListener<T extends PircBotX> extends ListenerAdapter<T> {
     }
 
 	/**
+	 * Eventually, this should read in the magic strings from some configuration file, and possibly also have a CLI. For now, we just connect to our favourite server and a test channel.
 	 * @param args
 	 */
     public static void main(String[] args) throws Exception {
-        //Configure what we want our bot to do
         Configuration<PircBotX> configuration = new Configuration.Builder<PircBotX>()
-                        .setName("bunnycat") //Set the nick of the bot. CHANGE IN YOUR CODE
-                        .setServerHostname("irc.elvum.net") //Join the freenode network
+                        .setName("bunnycat")
+                        .setServerHostname("irc.elvum.net")
                         .setServerPort(6697)
                         .setSocketFactory(new UtilSSLSocketFactory().trustAllCertificates())
-                        .addAutoJoinChannel("#gdsbottest") //Join the official #pircbotx channel
-                        .addListener(new BunnyListener<PircBotX>()) //Add our listener that will be called on Events
+                        .addAutoJoinChannel("#gdsbottest")
+                        .addListener(new BunnyListener<PircBotX>())
                         .buildConfiguration();
 
         //Create our bot with the configuration
